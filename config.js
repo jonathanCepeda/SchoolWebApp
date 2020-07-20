0be15882ae9febde36516d92ad97ba9cf69f6e99
jonathanCepeda <jonathan.cb@outlook.com>
2020-07-19 22:08:14 -0500

    last

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/schooldb";
-exports.PORT = process.env.PORT || 8080;
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/guitarsDB";
+exports.PORT = process.env.PORT || 3000;
